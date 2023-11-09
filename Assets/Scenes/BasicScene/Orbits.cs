17eba562bdb08ec280c726b313cdf3499d0289ff
Alyssa Blanco <alyssakblanco@outlook.com>
2023-11-08 22:32:07 -0700

    creating barriers and adjusting orbit speed

--- Assets/Scenes/BasicScene/Orbits.cs
+++ Assets/Scenes/BasicScene/Orbits.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Orbits : MonoBehaviour
 {
     public Transform sunTransform;
-    public float orbitSpeed = 10.0f;
+    public float orbitSpeed;
+
+    private float scaledDownBy = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,6 @@ public class Orbits : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(sunTransform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+        transform.RotateAround(sunTransform.position, Vector3.up, (orbitSpeed / scaledDownBy) * Time.deltaTime);
     }
 }
