ddea57089566308d47766194c8703dfc0ffe010a
Alyssa Blanco <alyssakblanco@outlook.com>
2023-11-10 20:50:28 -0700

    welcome message working

--- Assets/Scenes/BasicScene/WelcomeScript.cs
+++ Assets/Scenes/BasicScene/WelcomeScript.cs
@@ -5,35 +5,19 @@ using UnityEngine.UI;
 
 public class WelcomeScript : MonoBehaviour
 {
-    public Text welcomeMessageText_NEW2;
-    public Text descriptionText_NEW2;
     public GameObject canvas;
     // Start is called before the first frame update
     void Start()
     {
-        // ... existing code ...
-
-        // Set the welcome message and description
-        welcomeMessageText_NEW2.text = "Welcome to the Solar System Explorer!";
-        descriptionText_NEW2.text = "Interact with any planet to learn more about it. Use the primary button to view details and the secondary button to reset the view.";
-
-        // Optionally, hide the messages after a delay
-        StartCoroutine(HideWelcomeMessageAfterDelay(5)); // Hide after 5 seconds, for example
+        // Show welcome message for 5 seconds
+        StartCoroutine(HideWelcomeMessageAfterDelay(5)); 
 
 
     }
     IEnumerator HideWelcomeMessageAfterDelay(float delay)
     {
+        Debug.Log("here");
         yield return new WaitForSeconds(delay);
         canvas.gameObject.SetActive(false);
-        //welcomeMessageText.gameObject.SetActive(false);
-        //descriptionText.gameObject.SetActive(false);
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
