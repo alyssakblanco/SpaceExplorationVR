eeb56c6dbf233f572f8149bd0af77f8ac1b53097
Rachel-Wallace1 <r.wallacer12@gmail.com>
2023-12-14 23:52:30 -0800

    reset player position

--- Assets/Scenes/MainScene/Scripts/Orbits.cs
+++ Assets/Scenes/MainScene/Scripts/Orbits.cs
@@ -6,10 +6,13 @@ using UnityEngine.XR;
 public class Orbits : MonoBehaviour
 {
     public Transform sunTransform;
+    public Transform mainCameraTransform;
     public float orbitSpeed;
     public float rotationSpeed;
-    private Vector3 initialPosition;
-    private Quaternion initialRotation;
+    private Vector3 planetInitialPosition;
+    private Quaternion planetInitialRotation;
+    private Vector3 rigInitialPosition;
+    private Quaternion rigInitialRotation;
     private List<InputDevice> rightHandDevices = new List<InputDevice>();
     private float speedScaledBy = 10;
 
@@ -21,15 +24,27 @@ public class Orbits : MonoBehaviour
 
     void Start()
     {
-        initialPosition = transform.position;
-        initialRotation = transform.rotation;
+        planetInitialPosition = transform.position;
+        planetInitialRotation = transform.rotation;
+
+        Transform xrRig = mainCameraTransform.parent;
+        if (xrRig != null)
+        {
+            rigInitialPosition = xrRig.position;
+            rigInitialRotation = xrRig.rotation;
+        }
+        else
+        {
+            rigInitialPosition = mainCameraTransform.position;
+            rigInitialRotation = mainCameraTransform.rotation;
+        }
+
         GetDevices();
     }
 
     void Update()
     {
         transform.RotateAround(sunTransform.position, Vector3.up, (orbitSpeed / speedScaledBy) * Time.deltaTime);
-        // transform.RotateAround(Vector3.zero, Vector3.up, orbitSpeed * Time.deltaTime);
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
 
         if (rightHandDevices.Count == 0)
@@ -37,15 +52,33 @@ public class Orbits : MonoBehaviour
             GetDevices();
         }
 
-        // Press 'B' to reset planet positions
-        // TODO: reset player position as well
         foreach (var device in rightHandDevices)
         {
-            bool buttonPressed;
-            if (device.TryGetFeatureValue(CommonUsages.secondaryButton, out buttonPressed) && buttonPressed)
+            bool bButtonPressed;
+            bool aButtonPressed;
+
+            device.TryGetFeatureValue(CommonUsages.secondaryButton, out bButtonPressed);
+            device.TryGetFeatureValue(CommonUsages.primaryButton, out aButtonPressed);
+
+            if (bButtonPressed)
             {
-                transform.position = initialPosition;
-                transform.rotation = initialRotation;
+                transform.position = planetInitialPosition;
+                transform.rotation = planetInitialRotation;
+            }
+
+            if (aButtonPressed)
+            {
+                Transform xrRig = mainCameraTransform.parent;
+                if (xrRig != null)
+                {
+                    xrRig.position = rigInitialPosition;
+                    xrRig.rotation = rigInitialRotation;
+                }
+                else
+                {
+                    mainCameraTransform.position = rigInitialPosition;
+                    mainCameraTransform.rotation = rigInitialRotation;
+                }
             }
         }
     }
